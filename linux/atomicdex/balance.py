727d353d821d4d33818cd9abb0975adec8425013
smk762 <smk762@iinet.net.au>
2021-12-16 16:41:43 +0800

    get balance

--- linux/atomicdex/balance.py
+++ linux/atomicdex/balance.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from lib_atomicdex import *
 
-# Documentation: https://developers.komodoplatform.com/basic-docs/atomicdex-api-legacy/coin_activation.html#native-mode-activation
+# Documentation: https://developers.komodoplatform.com/basic-docs/atomicdex-api-legacy/my_balance.html
 
 if len(sys.argv) > 1:
 	for i in range(1,len(sys.argv)):
-		params = get_activate_command(sys.argv[i])
+		coin = sys.argv[i]
+		params = {"userpass":"$userpass","method":"my_balance","coin":coin}
 		if len(params) > 0:
 			resp = mm2_proxy(params)
 			print(resp)
@@ -13,4 +14,5 @@ if len(sys.argv) > 1:
 			print(f"{sys.argv[i]} is not a recognised coin!")
 else:
 	print(f"You need to add a coin (or series of coins) as a parameter")
-	print(f"e.g. ./activate.py KMD BTC TKL")
+	print(f"e.g. ./balance.py KMD BTC TKL")
+
