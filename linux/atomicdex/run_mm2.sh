dfc5d2eec14e9b596261d8879bd8d301f69d3ce2
smk762 <smk762@iinet.net.au>
2021-12-16 16:26:42 +0800

    add tail logs echo

--- linux/atomicdex/run_mm2.sh
+++ linux/atomicdex/run_mm2.sh
@@ -3,4 +3,4 @@ source userpass
 stdbuf -oL ./mm2 > mm2.log &
 sleep 3
 curl --url "http://127.0.0.1:7783" --data "{\"method\":\"version\",\"userpass\":\"$userpass\"}"
-echo ""
+echo "run 'tail -f mm2.log' to see the stdout logs for the AtomicDEX-API"
